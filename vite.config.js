53aa4e2f38a7d04e3699bfd5c4940f7da9cee475
Dinuk1994 <ddt94119@gmail.com>
2024-08-20 20:37:26 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base : "/Dinuk-Portfolio/",
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
   server: {
-    // Ensure CORS is handled if needed
     cors: true,
   },
-})
+});
